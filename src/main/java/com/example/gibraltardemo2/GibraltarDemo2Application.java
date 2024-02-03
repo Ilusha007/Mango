d5327f2ef9f08ff2c0cff4aab3d06d5184a9b54c
unknown <Student@305-6.spobip-bel.ru>
2024-02-03 14:11:27 +0300

    initial

---
+++ src/main/java/com/example/gibraltardemo2/GibraltarDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.gibraltardemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibraltarDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibraltarDemo2Application.class, args);
+	}
+
+}
